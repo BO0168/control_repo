8a251db7ebb930d86b0955964d6c3d00eaaf0aff
BO0168 <147069798+BO0168@users.noreply.github.com>
2024-01-08 14:33:02 +0100

    Update init.pp correct syntax issue with quotes

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,8 +1,8 @@
 class minecraft {
-  file {‘/opt/minecraft’:
+  file {'/opt/minecraft':
     ensure => directory,
   }
-  file {‘/opt/minecraft/server.jar’:
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source      => 'https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar',
   }
@@ -16,15 +16,15 @@ class minecraft {
     creates       => '/opt/minecraft/jdk-21.0.1',
     cleanup       => true,
   }
-  file {‘/opt/minecraft/eula.txt’:
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
-    content => ‘eula=true’
+    content => 'eula=true'
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
